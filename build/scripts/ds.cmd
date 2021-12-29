64ab58df9ee9b048633095bff89994997300fd40
Amey Subhedar <ameyasubhedar@gmail.com>
2021-12-29 11:32:27 -0800

    release pipeline

--- build/scripts/ds.cmd
+++ build/scripts/ds.cmd
@@ -1,4 +1,4 @@
 REM Build doxfc_project and then start the docfx server on 8080 port, lauch the browser and serve the pages
 call docfx docfx_project/docfx.json
-start chrome http://localhost:8080
+start http://localhost:8080
 call docfx serve docfx_project/_site
